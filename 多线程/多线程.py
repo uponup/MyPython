b4ef827d3d363f8c0bfcf2d5e06da022cb53998e
uponup <1030360567@qq.com>
2019-12-20 18:42:51 +0800

    线程

--- 多线程/多线程.py
+++ 多线程/多线程.py
@@ -34,3 +34,16 @@ thread1.join()
 thread2.join()
 
 print("退出主线程")
+
+
+# threading 内置方法的说明
+# threading.currentThread(): 返回当前的线程变量。
+# threading.enumerate(): 返回一个包含正在运行的线程的list。正在运行指线程启动后、结束前，不包括启动前和终止后的线程。
+# threading.activeCount(): 返回正在运行的线程数量，与len(threading.enumerate())有相同的结果。
+
+# run(): 用以表示线程活动的方法。
+# start():启动线程活动。
+# join([time]): 等待至线程中止。这阻塞调用线程直至线程的join() 方法被调用中止-正常退出或者抛出未处理的异常-或者是可选的超时发生。
+# isAlive(): 返回线程是否活动的。
+# getName(): 返回线程名。
+# setName(): 设置线程名。
---
+++ 多线程/线程同步.py
@@ -0,0 +1,43 @@
+import threading
+import time
+
+threadLock = threading.Lock()
+
+def print_time(threadName, delay, counter):
+    while counter:
+        time.sleep(delay)
+        print('%s %s' %(threadName, time.ctime(time.time())))
+        counter -= 1
+
+class myThread(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    
+    def run(self):
+        print("开启线程：", self.name)
+        # 获取锁，用于线程同步
+        threadLock.acquire()
+        print_time(self.name, self.counter, 3)
+
+        # 释放锁，开启下个线程
+        threadLock.release()
+
+
+threads = []
+
+thread1 = myThread(1, 'Thread-1', 1)
+thread2 = myThread(2, 'Thread-2', 2)
+
+thread1.start()
+thread2.start()
+
+threads.append(thread1)
+threads.append(thread2)
+
+for thread in threads:
+    thread.join()
+
+print('退出主线程')
